a9f79a16975d74f2e830d257d2257f5056abe114
Anthony Colangelo <me@acolangelo.com>
2017-08-10 11:14:29 -0400

    New bottom pieces to support breadboards.

--- code/flightcontrol/flightcontrol.ino
+++ code/flightcontrol/flightcontrol.ino
@@ -1,22 +1,22 @@
-int buttonPins[] = {10};
-int buttonButtons[] = {1};
+int buttonPins[] = {2};
+int buttonButtons[] = {2};
 
-int togglePins[] = {11, 12};
-int toggleButtons[] = {2, 3};
-int toggleStates[] = {0, 0};
+int togglePins[] = {26};
+int toggleButtons[] = {1};
+int toggleStates[] = {0};
 
 void setup() {
   // Assign button pins to INPUT_PULLUP
-  int i;
-  for (i = 0; i < 1; i = i + 1) {
-    pinMode(buttonPins[i], INPUT_PULLUP);
-  }
+//  int i;
+//  for (i = 0; i < 1; i = i + 1) {
+//    pinMode(buttonPins[i], INPUT_PULLUP);
+//  }
   
   // Assign toggle pins to INPUT
-  int j;
-  for (j = 0; j < 1; j = j + 1) {
-    pinMode(togglePins[j], INPUT);
-  }
+//  int j;
+//  for (j = 0; j < 1; j = j + 1) {
+//    pinMode(togglePins[j], INPUT);
+//  }
 }
 
 void loop() {
@@ -27,24 +27,29 @@ void loop() {
   // }
   
   // Read buttons
-  int i;
-  for (i = 0; i < 1; i = i + 1) {
-    Joystick.button(buttonButtons[i], !digitalRead(buttonPins[i]));
-  }
+//  int i;
+//  for (i = 0; i < 2; i = i + 1) {
+//    Joystick.button(buttonButtons[i], !digitalRead(buttonPins[i]));
+//  }
   
   // Read toggle switches
-  int j;
-  for (j = 0; j < 1; j = j + 1) {
-    int currentState = digitalRead(togglePins[j]);
-    Joystick.button(toggleButtons[j], (currentState != toggleStates[j]));
-    toggleStates[j] = currentState;
-  }
+//  int j;
+//  for (j = 0; j < 1; j = j + 1) {
+//    int currentState = digitalRead(togglePins[j]);
+//    Joystick.button(toggleButtons[j], (currentState != toggleStates[j]));
+//    toggleStates[j] = currentState;
+//  }
   
   // read analog inputs and set X-Y position
   // Joystick.Y(analogRead(0));
-  // Joystick.X(analogRead(1));
+  // Joystick.Y(analogRead(1));
   // Joystick.Z(analogRead(2));
-  // Joystick.sliderLeft(analogRead(4));
-  delay(50);
+  // Joystick.sliderLeft(analogRead(6));
+
+  Joystick.Zrotate(analogRead(4));
+  
+  // Joystick.button(31, (analogRead(4) < 341));
+  // Joystick.button(32, (analogRead(4) > 680));
+  delay(10);
 }
 
