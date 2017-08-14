4a365bfcdea3dfbec1e712ccaa5cb4d9ad0f9214
Anthony Colangelo <me@acolangelo.com>
2017-08-14 11:23:12 -0400

    Completed middle panel circuits.

--- code/flightcontrol/flightcontrol.ino
+++ code/flightcontrol/flightcontrol.ino
@@ -1,22 +1,27 @@
-int buttonPins[] = {2};
-int buttonButtons[] = {2};
+int buttonPins[]    = {0,   2,  3};
+int buttonButtons[] = {11,  14, 15};
 
-int togglePins[] = {26};
-int toggleButtons[] = {1};
-int toggleStates[] = {0};
+int togglePins[]    = {27,  1};
+int toggleButtons[] = {13,  12};
+int toggleStates[]  = {0,   0};
 
 void setup() {
   // Assign button pins to INPUT_PULLUP
-//  int i;
-//  for (i = 0; i < 1; i = i + 1) {
-//    pinMode(buttonPins[i], INPUT_PULLUP);
-//  }
+  int i;
+  for (i = 0; i < 3; i = i + 1) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
   
   // Assign toggle pins to INPUT
-//  int j;
-//  for (j = 0; j < 1; j = j + 1) {
-//    pinMode(togglePins[j], INPUT);
-//  }
+  int j;
+  for (j = 0; j < 2; j = j + 1) {
+    // Control Mode needs resistor enabled.
+    if (togglePins[j] == 27) {
+      pinMode(togglePins[j], INPUT_PULLUP);
+    } else {
+      pinMode(togglePins[j], INPUT);
+    }
+  }
 }
 
 void loop() {
@@ -27,29 +32,73 @@ void loop() {
   // }
   
   // Read buttons
-//  int i;
-//  for (i = 0; i < 2; i = i + 1) {
-//    Joystick.button(buttonButtons[i], !digitalRead(buttonPins[i]));
-//  }
+  int i;
+  for (i = 0; i < 3; i = i + 1) {
+    Joystick.button(buttonButtons[i], !digitalRead(buttonPins[i]));
+  }
   
   // Read toggle switches
-//  int j;
-//  for (j = 0; j < 1; j = j + 1) {
-//    int currentState = digitalRead(togglePins[j]);
-//    Joystick.button(toggleButtons[j], (currentState != toggleStates[j]));
-//    toggleStates[j] = currentState;
-//  }
+  int j;
+  for (j = 0; j < 2; j = j + 1) {
+    int currentState = digitalRead(togglePins[j]);
+    if (toggleButtons[j] == 12) {
+      if (currentState == toggleStates[j]) {
+        continue;
+      }
+      // Staging Lock
+      Keyboard.begin();
+      Keyboard.press(KEY_LEFT_ALT);
+      delay(10);
+      Keyboard.press('l');
+      delay(1);
+      Keyboard.release('l');
+      delay(1);
+      Keyboard.release(KEY_LEFT_ALT);
+      Keyboard.end();
+    } else  if (toggleButtons[j] == 13) {
+      // Control Mode
+      if (currentState == toggleStates[j]) {
+        continue;
+      }
+      Keyboard.begin();
+      if (currentState == 0) {
+        // Flight
+        Keyboard.press('y');
+        delay(1);
+        Keyboard.release('y');
+      } else if (currentState == 1) {
+        // Docking
+        Keyboard.press(KEY_DELETE);
+        delay(1);
+        Keyboard.release(KEY_DELETE);
+      }
+      Keyboard.end();
+    } else {
+      Joystick.button(toggleButtons[j], (currentState != toggleStates[j]));
+    }
+    toggleStates[j] = currentState;
+  }
   
-  // read analog inputs and set X-Y position
-  // Joystick.Y(analogRead(0));
-  // Joystick.Y(analogRead(1));
-  // Joystick.Z(analogRead(2));
-  // Joystick.sliderLeft(analogRead(6));
+  // Right Joystick
+  Joystick.X(analogRead(0));
+  Joystick.Y(analogRead(1));
+  Joystick.Z(analogRead(2));
 
-  Joystick.Zrotate(analogRead(4));
+  // Left Joystick
+  Joystick.Zrotate(analogRead(3));
+  Joystick.sliderRight(analogRead(4));
+  Joystick.button(31, (analogRead(5) < 341));
+  Joystick.button(32, (analogRead(5) > 680));
+
+  // Throttle
+  int throttleAmount = analogRead(6);
+  if (throttleAmount < 40) {
+    throttleAmount = 0;
+  } else if (throttleAmount > 1010) {
+    throttleAmount = 1023;
+  }
+  Joystick.sliderLeft(throttleAmount);
   
-  // Joystick.button(31, (analogRead(4) < 341));
-  // Joystick.button(32, (analogRead(4) > 680));
   delay(10);
 }
 
